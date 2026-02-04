8be12cefbe3329895f4b82c6a3d4801b67c05a3f
vaibhav <sisodevaibhav11@gmail.com>
2026-02-05 00:23:51 +0530

    Add owner dashboard and management components with routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,11 @@ import Cars from './pages/Cars.jsx'
 import CarDetails from './pages/CarDetails.jsx'
 import MyBookings from './pages/MyBookings.jsx'
 import Footer from './components/Footer.jsx'
-
+import Layout from './pages/owner/Layout.jsx'
+import Dashboard from './pages/owner/Dashboard.jsx'
+import AddCar from './pages/owner/AddCar.jsx'
+import ManageCars from './pages/owner/ManageCars.jsx'
+import ManageBookings from './pages/owner/ManageBookings.jsx'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -20,6 +24,12 @@ const App = () => {
         <Route path='/car/:id' element={<CarDetails />} />
         <Route path='/cars' element={<Cars />} />
         <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/owner' element={<Layout />} >  
+          <Route index element={<Dashboard  />} />
+          <Route path='add-car' element={<AddCar />} />
+          <Route path='manage-cars' element={<ManageCars />} />
+          <Route path='manage-bookings' element={<ManageBookings />} />
+        </Route>
       </Routes>
 
       {!isOwnerPath && <Footer/>}
